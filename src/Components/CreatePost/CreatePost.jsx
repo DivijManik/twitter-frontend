8f9c4a822e9c18a3821913f29fd962362e37b10b
DivijManik <51446479+DivijManik@users.noreply.github.com>
2024-12-10 18:21:36 +0530

    tweet character limit added

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -4,6 +4,7 @@ import PersonImg from '../Images/person.svg'
 import CloseImg from '../Images/close.svg'
 
 function CreatePost({ SetCreatePost }) {
+    const [tweet, setTweet] = useState("");
     return (
         <div className='CreatePostParent'>
             <div className='CreatePostTop'>
@@ -12,7 +13,8 @@ function CreatePost({ SetCreatePost }) {
             </div>
             {/* User Img */}
             <img className='UserImg_cpost' src={PersonImg}></img>
-            <textarea className='Input_cpost' placeholder="What's Happening?"></textarea>
+            <textarea className='Input_cpost' placeholder="What's Happening?" value={tweet} onChange={e => e.target.value.length <= 500 ? setTweet(e.target.value) : {}}></textarea>
+            <p style={{ position: "relative", top: 50, left: 80, color: "gray" }}>{tweet.length}/500</p>
         </div>
     )
 }
